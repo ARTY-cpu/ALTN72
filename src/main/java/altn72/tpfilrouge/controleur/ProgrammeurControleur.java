c1b813b4525c56e08e2c1e98277fd24944b3e6db
Arthur Magnette <amagnette@magicsoftware.com>
2025-10-02 12:38:25 +0200

    CRUD ajouté

--- src/main/java/altn72/tpfilrouge/controleur/ProgrammeurControleur.java
+++ src/main/java/altn72/tpfilrouge/controleur/ProgrammeurControleur.java
@@ -1,23 +1,48 @@
 package altn72.tpfilrouge.controleur;
 
 import altn72.tpfilrouge.modele.Programmeur;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
 @RequestMapping("/tpfilrouge")
 public class ProgrammeurControleur {
-    @Autowired
-    ProgrammeurService programmeurService;
-    
+
+
+    //@Autowired
+    private final ProgrammeurService programmeurService;
+
+    // cela remplace autowired le constructeur
+    public ProgrammeurControleur(ProgrammeurService programmeurService) {
+        this.programmeurService = programmeurService;
+    }
+
     @GetMapping("/hello")
     public List<Programmeur>afficherProgrammeurs(){
         return programmeurService.getProgrammeurs();
 
+    }
+
+    @GetMapping("/unProgrammeur/{idProgrammeur}")
+    public Optional<Programmeur> afficherUnProgrammeur(@PathVariable Integer idProgrammeur){
+        return programmeurService.getunProgrammeur(idProgrammeur);
+    }
+
+    @DeleteMapping("/suppression/{idProgrammeur}")
+    public String suppression(@PathVariable Integer idProgrammeur){
+        return programmeurService.deleteProgrammeur(idProgrammeur);
+    }
+
+    @PostMapping("/ajout/")
+    public Programmeur ajouter(@RequestBody Programmeur programmeur){
+        return  programmeurService.saveProgrammeur(programmeur);
+    }
+
+    @PutMapping("/modification/{idProgrammeur}")
+    public Programmeur modifier(@PathVariable Integer idProgrammeur, @RequestBody Programmeur programmeur){
+        return programmeurService.updateProgrammeur(idProgrammeur, programmeur);
     }
 }
--- src/main/java/altn72/tpfilrouge/controleur/ProgrammeurService.java
+++ src/main/java/altn72/tpfilrouge/controleur/ProgrammeurService.java
@@ -2,18 +2,48 @@ package altn72.tpfilrouge.controleur;
 
 import altn72.tpfilrouge.modele.Programmeur;
 import altn72.tpfilrouge.modele.ProgrammeurRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProgrammeurService {
 
-    @Autowired
-    ProgrammeurRepository programmeurRepository;
+    //@Autowired
+    private final ProgrammeurRepository programmeurRepository;
+
+    public ProgrammeurService(ProgrammeurRepository programmeurRepository) {
+        this.programmeurRepository = programmeurRepository;
+    }
 
     public List<Programmeur> getProgrammeurs(){
         return programmeurRepository.findAll();
+    }
+
+    public Optional<Programmeur> getunProgrammeur(Integer idProgrammeur){
+        return programmeurRepository.findById(idProgrammeur);
+    }
+
+    public String deleteProgrammeur(Integer idProgrammeur){
+        try {
+            programmeurRepository.deleteById(idProgrammeur);
+            return ("Programmeur supprimé avec succès");
+        }
+        catch (Exception e){
+            throw new RuntimeException("Programmeur inexistant");
+        }
+    }
+
+    public Programmeur saveProgrammeur(Programmeur programmeur){
+        return programmeurRepository.save(programmeur);
+    }
+
+    public Programmeur updateProgrammeur(Integer idProgrammeur, Programmeur programmeur){
+        if (programmeurRepository.existsById(idProgrammeur)){
+            deleteProgrammeur(idProgrammeur);
+            return saveProgrammeur(programmeur);
+        }
+        throw new RuntimeException("Programmeur inexistant");
     }
 }
